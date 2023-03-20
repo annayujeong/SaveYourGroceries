e237ecbf19222a4b7a3915789aa6c560719047a7
Kristopher Ocampo <kris.ocampo@hotmail.com>
2023-03-19 21:45:00 -0700

    Second iteration of SavedItemsList that was based off of Bradner's Json file parser that reads a Json file and displays information.

---
+++ SaveYourGroceries/SavedGroceryItem.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace SaveYourGroceries
+{
+    partial class SavedGroceryItem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.savedItemListRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.buttonReadJson = new System.Windows.Forms.Button();
+            this.buttonWriteJson = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // savedItemListRichTextBox
+            // 
+            this.savedItemListRichTextBox.Location = new System.Drawing.Point(93, 259);
+            this.savedItemListRichTextBox.Name = "savedItemListRichTextBox";
+            this.savedItemListRichTextBox.Size = new System.Drawing.Size(350, 519);
+            this.savedItemListRichTextBox.TabIndex = 0;
+            this.savedItemListRichTextBox.Text = "";
+            // 
+            // buttonReadJson
+            // 
+            this.buttonReadJson.Location = new System.Drawing.Point(54, 141);
+            this.buttonReadJson.Name = "buttonReadJson";
+            this.buttonReadJson.Size = new System.Drawing.Size(220, 54);
+            this.buttonReadJson.TabIndex = 1;
+            this.buttonReadJson.Text = "Read Json File";
+            this.buttonReadJson.UseVisualStyleBackColor = true;
+            this.buttonReadJson.Click += new System.EventHandler(this.buttonReadJSONFile_OnClick);
+            // 
+            // buttonWriteJson
+            // 
+            this.buttonWriteJson.Location = new System.Drawing.Point(304, 141);
+            this.buttonWriteJson.Name = "buttonWriteJson";
+            this.buttonWriteJson.Size = new System.Drawing.Size(220, 54);
+            this.buttonWriteJson.TabIndex = 2;
+            this.buttonWriteJson.Text = "Write Json File";
+            this.buttonWriteJson.UseVisualStyleBackColor = true;
+            this.buttonWriteJson.Click += new System.EventHandler(this.buttonWriteJSON_Click);
+            // 
+            // SavedGroceryItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 1131);
+            this.Controls.Add(this.buttonWriteJson);
+            this.Controls.Add(this.buttonReadJson);
+            this.Controls.Add(this.savedItemListRichTextBox);
+            this.Name = "SavedGroceryItem";
+            this.Text = "SavedGroceryItem";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox savedItemListRichTextBox;
+        private System.Windows.Forms.Button buttonReadJson;
+        private System.Windows.Forms.Button buttonWriteJson;
+    }
+}
---
+++ SaveYourGroceries/SavedGroceryItem.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace SaveYourGroceries
+{
+    public partial class SavedGroceryItem : Form
+    {
+        private readonly string _path = // $"\"C:\\Users\\kriso\\Downloads\\SavedGroceryStoreItemList.json";
+                                        $"\"C:\\Users\\kriso\\source\\repos\\annayujeong\\SaveYourGroceries\\SaveYourGroceries\\SavedGroceryStoreItemList.json";
+
+        public SavedGroceryItem()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonReadJSONFile_OnClick(object sender, EventArgs e)
+        {
+          try 
+          {
+           string jsonFromFile;
+           using (var reader = new StreamReader(_path))
+           {
+            jsonFromFile = reader.ReadToEnd();
+           }
+
+          // Richtextbox refers to the Designer element rich textbox https://www.c-sharpcorner.com/UploadFile/mahesh/richtextbox-in-C-Sharp/
+                savedItemListRichTextBox.Text = jsonFromFile;
+           var savedItemInfoFromJson = JsonConvert.DeserializeObject<SavedGroceryStoreItemList>(jsonFromFile);
+           
+          }
+            catch (Exception ex)
+            {
+               MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonWriteJSON_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var savedGroceryStoreItem = GetInfoOfSavedItems();
+                var jsonToWrite = JsonConvert.SerializeObject(savedGroceryStoreItem, Formatting.Indented);
+                using(var writer = new StreamWriter(_path))
+                {
+                    writer.Write(jsonToWrite);
+                }
+            }
+
+            catch (Exception ex) 
+            { 
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private SavedGroceryStoreItem GetInfoOfSavedItems()
+        {
+            var savedGroceryStoreItem = new SavedGroceryStoreItem
+            {
+                     savedItemName = "Apple",
+                     savedItemPrice = "3.99",
+                     savedItemStoreName = "Superstore",
+                     savedItemImageUrl = "https://upload.wikimedia.org/wikipedia/commons/0/07/Whole_apple_and_bitten_apple.jpg"
+
+            };
+
+            return savedGroceryStoreItem;
+        }
+
+
+        //private SavedGroceryStoreItemList GetInfoOfSavedItems()
+        //{
+        //    var savedGroceryStoreItem = new SavedGroceryStoreItem
+        //    {
+        //        SavedListOfItems = new List<SavedGroceryStoreItem>
+        //        {
+        //            new SavedGroceryStoreItem
+        //            {
+        //             savedItemName = "Apple",
+        //             savedItemPrice = "3.99",
+        //             savedItemStoreName = "Superstore",
+        //             savedItemImageUrl = ""
+        //            }
+        //        }
+
+
+        //    };
+
+        //    return savedGroceryStoreItem;
+        //}
+
+
+    }
+
+    public class SavedGroceryStoreItem
+    {
+        public string savedItemName { get; set; }
+        public string savedItemPrice { get; set; }
+
+        public string savedItemStoreName { get; set; }
+
+        public string savedItemImageUrl { get; set; }
+        public List<SavedGroceryStoreItem> SavedListOfItems { get; internal set; }
+    }
+}
+
+
